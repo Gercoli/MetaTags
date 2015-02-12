7f4b471895fa4762c9fc831e08a975fb0060bd80
Garry Ercoli <Garry@GErcoli.com>
2015-02-12 00:43:43 -0600

    re-thinking how this class will work, created small interface

--- src/MetaTags.php
+++ src/MetaTags.php
@@ -1,5 +1,26 @@
 <?php namespace GErcoli\MetaTags;
 
-class MetaTags {
+class MetaTags implements MetaTagsInterface {
 
+    /**
+     * Truncate a given string at the end of the word closest (but not past) to the max length
+     * @param   string  $string
+     * @param   int     $maximum_length
+     * @return  string
+     */
+    private function truncateAtWord($string, $maximum_length = 120)
+    {
+        $string = trim(substr($string, 0, $maximum_length + 1),", \t\n\r\0\x0B");
+        return (strlen($string) > $maximum_length) ? preg_replace('/\s+?(\S+)?$/', '', $string) : $string;
+    }
+
+    /**
+     * Removes multiple consecutive spaces and replaces it with a single space.
+     * @param   string  $string
+     * @return  string
+     */
+    private function removeMultipleSpaces($string)
+    {
+        return preg_replace('/[ ]{2,}/', ' ', $string);
+    }
 }
---
+++ src/MetaTagsInterface.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Garry
+ * Date: 2/11/15
+ * Time: 9:21 PM
+ */
+
+namespace GErcoli\MetaTags;
+
+
+interface MetaTagsInterface {
+    function __construct();
+    public static function getInstance();
+    public static function title($page_title);
+    public static function description();
+    public static function keywords();
+    public static function author();
+    public static function charset($charset = "UTF-8");
+    public static function refresh($seconds = 0);
+    public static function appleTouchIcon($resolution, $icon_url, $precomposed = true); // http://stackoverflow.com/questions/5110776/apple-touch-icon-for-websites
+    public static function viewport($extra_options = "");  // <meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, user-scalable=yes">
+    public static function phoneLinking($enable = true);    // <meta name="format-detection" content="telephone=no" /> <meta name="x-rim-auto-match" http-equiv="x-rim-auto-match" forua="true" content="none"/>
+}
