0ff38f6e45540b89075cb5d5bbe39639dd43a14f
Garry Ercoli <Garry@GErcoli.com>
2015-02-12 01:25:41 -0600

    Created singleton and phpunit test for it

--- src/MetaTags.php
+++ src/MetaTags.php
@@ -3,6 +3,87 @@
 class MetaTags implements MetaTagsInterface {
 
     /**
+     * @var static
+     */
+    protected static $instance = null;
+
+    /**
+     * Singleton constructor, only accessible internally by getInstance()
+     * @see getInstance()
+     */
+    final private function __construct()
+    {
+        // No implementation.
+    }
+
+    /**
+     * Singleton clone method, to stop the object from being cloned.
+     */
+    final private function __clone()
+    {
+        // No implementation.
+    }
+
+    /**
+     * Returns the instance of this class, if one doesn't exist, it creates one first.
+     * @return MetaTags|static
+     */
+    public static function getInstance()
+    {
+        if(!(static::$instance instanceof static))
+        {
+            self::$instance = new static();
+        }
+
+        return self::$instance;
+    }
+
+    public static function title($page_title)
+    {
+        // TODO: Implement title() method.
+    }
+
+    public static function description()
+    {
+        // TODO: Implement description() method.
+    }
+
+    public static function keywords()
+    {
+        // TODO: Implement keywords() method.
+    }
+
+    public static function author()
+    {
+        // TODO: Implement author() method.
+    }
+
+    public static function charset($charset = "UTF-8")
+    {
+        // TODO: Implement charset() method.
+    }
+
+    public static function refresh($seconds = 0)
+    {
+        // TODO: Implement refresh() method.
+    }
+
+    public static function appleTouchIcon($resolution, $icon_url, $precomposed = true)
+    {
+        // TODO: Implement appleTouchIcon() method.
+    }
+
+    public static function viewport($extra_options = "")
+    {
+        // TODO: Implement viewport() method.
+    }
+
+    public static function phoneLinking($enable = true)
+    {
+        // TODO: Implement phoneLinking() method.
+    }
+
+    /**
      * Truncate a given string at the end of the word closest (but not past) to the max length
      * @param   string  $string
      * @param   int     $maximum_length
--- tests/testMetaTags.php
+++ tests/testMetaTags.php
@@ -1,6 +1,14 @@
 <?php
-use GErcoli\MetaTags\MetaTags;
+use GErcoli\MetaTags\MetaTags as MetaTags;
 
 class HTMLTagTest extends PHPUnit_Framework_TestCase {
-    
+    public function testCreateInstance()
+    {
+        $a = MetaTags::getInstance();
+        $b = MetaTags::getInstance();
+
+        $this->assertInstanceOf('GErcoli\MetaTags\HTMLTagTest',$a,"First instance is NOT an instance of 'MetaTags'.");
+        $this->assertInstanceOf('GErcoli\MetaTags\MetaTags',$b,"Second instance is NOT an instance of 'MetaTags'.");
+        $this->assertTrue($a === $b, "getInstance() returned two different instance.");
+    }
 }
